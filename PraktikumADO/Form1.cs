67f15dd976f4109cbcb68be036ec4874172cef74
tari2025 <denilestari272@gmail.com>
2026-03-13 19:18:53 +0700

    membuat btnInsertProdi_Click

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -1,6 +1,14 @@
 ﻿using System;
-using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+
 
 namespace PraktikumADO
 {
