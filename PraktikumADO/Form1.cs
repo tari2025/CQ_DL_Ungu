0030cb00b61743fcdb6b05ffeb5d5d72f6b7ccef
tari2025 <denilestari272@gmail.com>
2026-03-13 18:29:21 +0700

    hitung jumlah mata kuliah

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -67,3 +67,29 @@ namespace PraktikumADO
                 MessageBox.Show(ex.Message);
             }
         }
+
+        // Hitung jumlah Mata Kuliah
+        private void btnHitungMK_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Koneksi();
+                conn.Open();
+
+                string query = "SELECT COUNT(*) FROM MataKuliah";
+
+                cmd = new SqlCommand(query, conn);
+
+                int jumlah = (int)cmd.ExecuteScalar();
+
+                txtHasil.Text = jumlah.ToString();
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        
