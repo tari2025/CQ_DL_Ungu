be3af0cea8cd18274b1517b92afc495b80dc0ad3
tari2025 <denilestari272@gmail.com>
2026-03-13 19:09:48 +0700

    button insert program studi

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -162,6 +162,28 @@ namespace PraktikumADO
                 MessageBox.Show(ex.Message);
             }
         }
+        private void btnInsertProdi_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Koneksi();
+                conn.Open();
+
+                string query = "INSERT INTO ProgramStudi VALUES ('MI01','Manajemen Informatika')";
+
+                cmd = new SqlCommand(query, conn);
+
+                int hasil = cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Data berhasil ditambahkan");
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
 
     }
     }
