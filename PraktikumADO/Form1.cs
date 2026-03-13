e401f29a35feab5f886640aeddae961c01e759c4
tari2025 <denilestari272@gmail.com>
2026-03-13 18:29:46 +0700

    update data mahasiswa

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -92,4 +92,33 @@ namespace PraktikumADO
             }
         }
 
-        
+        // Update data
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Koneksi();
+                conn.Open();
+
+                string query = "UPDATE Mahasiswa SET Alamat='Yogyakarta' WHERE NIM='23110100001'";
+
+                cmd = new SqlCommand(query, conn);
+
+                int hasil = cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Jumlah baris terpengaruh: " + hasil);
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void txtHasil_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
